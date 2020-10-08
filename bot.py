37d70fac36616f649549122dfc4f6bfc3be1b0da
LinusCebulla1208 <59658701+LinusCebulla1208@users.noreply.github.com>
2020-10-08 21:38:26 +0200

    Balance Befehl schöner formatiert, Bugfixes

--- bot.py
+++ bot.py
@@ -8,11 +8,22 @@ client = cmd.Bot(command_prefix="-")
 async def on_ready():
   print(f"{client.user.name} ist nun online!")
 
-@client.command
-async def balance(ctx, equation):
+@client.command(name="balance")
+async def balance_(ctx, equation):
   edukte = equation.split("=")[0].replace(" ", "").split("+")
   produkte = equation.split("=")[1].replace(" ", "").split("+")
-  reac, prod = balance()
-  await ctx.send(f"{str(dict(reac))} = {str(dict(prod))}")
+  reac, prod = balance(edukte, produkte)
+  
+  reac_nice = f"{reac[list(reac.keys())[0]]}{list(reac.keys())[0]}"
+  for i in range(len(list(reac.keys()))-1):
+    reac_nice += f" + {list(reac.keys())[i+1]}"
+  
+  prod_nice = f"{prod[list(prod.keys())[0]]}{list(prod.keys())[0]}"
+  for i in range(len(list(prod.keys()))-1):
+    prod_nice += f" + {list(prod.keys())[i+1]}"
+  
+  #await ctx.send(f"{str(dict(reac))} = {str(dict(prod))}")
+  await ctx.send(f"{reac_nice} = {prod_nice}")
+
 
 client.run("NzYzODM0OTU4NDIyMjEyNjA4.X39evQ.FzxNFwqe5Z4jHYKGB425V5U4xdE")

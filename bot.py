c5ac2fe553d2026f481a6782dd6e9d13b35b1ae2
LinusCebulla1208 <59658701+LinusCebulla1208@users.noreply.github.com>
2020-10-08 21:00:07 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+import discord
+from chempy import balance_stoichiometry as balance
 
+client = cmd.Bot(command_prefix="-")
+
+@client.event
+async def on_ready():
+  print(f"{client.user.name} ist nun online!")
+
+@client.command
+async def balance(ctx, equation):
+  edukte = equation.split("=")[0].replace(" ", "").split("+")
+  produkte = equation.split("=")[1].replace(" ", "").split("+")
+  reac, prod = balance()
+  await ctx.send(f"{str(dict(reac))} = {str(dict(prod))}")
+
+client.run("NzYzODM0OTU4NDIyMjEyNjA4.X39evQ.FzxNFwqe5Z4jHYKGB425V5U4xdE")
